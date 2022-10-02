e7ad4dadf8db501809d6b07222f418728862994c
Miron Brezuleanu <mbrezu@gmail.com>
2022-10-02 19:13:01 +0300

    Tweaked control.

--- godot/Player.gd
+++ godot/Player.gd
@@ -7,7 +7,6 @@ signal died
 var x = 0
 var y = 0
 var maze = maze
-var velocity = Vector2.ZERO
 var target_position = Vector3.ZERO
 var speed = 3
 var face_up_rotation
@@ -62,44 +61,47 @@ func _process(delta):
 	if (translation - target_position).length() < 0.1:
 		translation = target_position
 
-	if Input.is_action_pressed("ui_left"):
-		velocity = Vector2.LEFT
-	elif Input.is_action_pressed("ui_right"):
-		velocity = Vector2.RIGHT
-	elif Input.is_action_pressed("ui_up"):
-		velocity = Vector2.UP
-	elif Input.is_action_pressed("ui_down"):
-		velocity = Vector2.DOWN
+	if translation == target_position:
+		var velocity = Vector2.ZERO
 
-	if translation == target_position and velocity != Vector2.ZERO:
-		var cell = maze.cells[x][y]
+		if Input.is_action_pressed("ui_left"):
+			velocity = Vector2.LEFT
+		elif Input.is_action_pressed("ui_right"):
+			velocity = Vector2.RIGHT
+		elif Input.is_action_pressed("ui_up"):
+			velocity = Vector2.UP
+		elif Input.is_action_pressed("ui_down"):
+			velocity = Vector2.DOWN
 
-		if velocity.x < 0:
-			$Body.rotation = face_up_rotation
-			$Body.rotate(Vector3.UP, PI / 2)
-			if !cell.has_left_wall or can_go_through_walls:
-				x -= 1
+		if velocity != Vector2.ZERO:
+			var cell = maze.cells[x][y]
 
-		if velocity.x > 0:
-			$Body.rotation = face_up_rotation
-			$Body.rotate(Vector3.UP, -PI / 2)
-			if !cell.has_right_wall or can_go_through_walls:
-				x += 1
+			if velocity.x < 0:
+				$Body.rotation = face_up_rotation
+				$Body.rotate(Vector3.UP, PI / 2)
+				if !cell.has_left_wall or can_go_through_walls:
+					x -= 1
 
-		if velocity.y < 0:
-			$Body.rotation = face_up_rotation
-			if !cell.has_top_wall or can_go_through_walls:
-				y -= 1
+			if velocity.x > 0:
+				$Body.rotation = face_up_rotation
+				$Body.rotate(Vector3.UP, -PI / 2)
+				if !cell.has_right_wall or can_go_through_walls:
+					x += 1
 
-		if velocity.y > 0:
-			$Body.rotation = face_up_rotation
-			$Body.rotate(Vector3.UP, PI)
-			if !cell.has_bottom_wall or can_go_through_walls:
-				y += 1
+			if velocity.y < 0:
+				$Body.rotation = face_up_rotation
+				if !cell.has_top_wall or can_go_through_walls:
+					y -= 1
 
-		x = clamp(x, 0, maze.cells.size() - 1)
-		y = clamp(y, 0, maze.cells.size() - 1)
-		set_player_position()
+			if velocity.y > 0:
+				$Body.rotation = face_up_rotation
+				$Body.rotate(Vector3.UP, PI)
+				if !cell.has_bottom_wall or can_go_through_walls:
+					y += 1
+
+			x = clamp(x, 0, maze.cells.size() - 1)
+			y = clamp(y, 0, maze.cells.size() - 1)
+			set_player_position()
 
 	var direction = (target_position - translation).normalized()
 	translation += direction * delta * speed
